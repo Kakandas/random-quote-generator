9f5f2d4623f737d1c90e9202eee2f32a27f402e2
Fakhri Rasyad <kaizerd23@gmail.com>
2023-01-03 15:00:37 +0800

    03/01/2023 update terakhir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState} from 'react';
+import Tumblr from './tumblr.svg';
+import Twitter from './twitter.svg';
 import './App.css';
 
 function App() {
+  const [quotes, setQuotes] = useState([])
+  
+  
+
+  const newQuote = () => {
+       fetch('https://api.quotable.io/random')
+       .then(response => response.json())
+       .then(data => setQuotes(data));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-box">
+      <div id='quotes'>
+        <p id='text'>{quotes.content}</p>
+        <p id="author">-{quotes.author}</p>
+      </div>
+      <div id='buttons'>
+        <div id='social-media'>
+          <a href='https://twitter.com/intent/tweet' target='_blank' rel='noreferrer'><img src={Twitter} alt='Twitter'></img></a>
+          <a href='https://tumblr.com' target='_blank' rel='noreferrer'><img src={Tumblr} alt='Tumblr'></img></a>
+        </div>
+        <div id='new-quote' onClick={() => {newQuote()}}>
+              <p>New Quote</p>
+        </div>
+      </div>
     </div>
   );
 }
